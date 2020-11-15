aa18c79060e372d51aa67a1e937b9ed39807bfeb
Lauren Burkett <LaurenBurkett@Laurens-MacBook-Air.local>
2020-11-15 09:53:44 -0500

    Integer Division Script

---
+++ ITS320_CTA1_Option2.py
@@ -0,0 +1,10 @@
+print('This program is designed to provide integer division, float division, and modulo division/operator.')
+print('Please choose two numbers:')
+number_1 = int(input())
+number_2 = int(input())
+answer_1 = number_1 // number_2
+print('Integer Division = ', answer_1)
+answer_2 = number_1 / number_2
+print('Float Division   = ', answer_2)
+answer_3 = number_1 % number_2
+print('Modulo Operator  = ', answer_3)
